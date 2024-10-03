c42a9b3c6b4816577fcbbb76bab38be87ef6fd6c
GregieBoi <mcgreggorkennison@gmail.com>
2024-10-03 12:33:25 -0500

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import { url } from "inspector";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://utfs.io/f/RM4zbiZHj0e8NTLhb25hpBFnQfTeD2t7gZqxz5UHujvIWPyL",
+  "https://utfs.io/f/RM4zbiZHj0e8bVTeaNwtPiOSU1LxbapWM9VgoI2kKeZs3NEG",
+  "https://utfs.io/f/RM4zbiZHj0e8QaOe3pNgZ5EGAF7DCfbisIoXHaTp2NLzSx1M",
+  "https://utfs.io/f/RM4zbiZHj0e86rKTA2XFphnYr8fWyaKdeTD9Ao0EcC16ZwG2"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Gallery In Progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {
+          [...mockImages, ...mockImages, ...mockImages].map((image) => (
+            <div key={image.id} className="w-48 p-4">
+              <img src={image.url} alt="image"/>
+            </div>
+          ))
+        }
+      </div>
     </main>
   );
 }
